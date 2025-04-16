f028ef17e94f47393c3803e5b2b60151ac6f674e
Linnet Cosmos Tuscano <71842687+linnettuscano@users.noreply.github.com>
2025-04-16 15:41:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from utils.logger import setup_logger
 logger = setup_logger("Main")
 
 def main():
+    
     try:
         logger.info("Starting Azure IoT Hub MQTT Publisher...")
         mqtt_azure_publisher.run()
+        
     except Exception as e:
         logger.error("Error in main: %s", str(e))
 
